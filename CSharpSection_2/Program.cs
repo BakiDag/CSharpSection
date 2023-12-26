dc73330dab8832df4f76b500d6cdafbcd20e4313
BakiDag <73464615+BakiDag@users.noreply.github.com>
2023-12-27 00:09:38 +0100

    [DEV] create class with static methods

--- CSharpSection_2/Program.cs
+++ CSharpSection_2/Program.cs
@@ -6,7 +6,27 @@ namespace CSharpSection_2
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine(SimpleMath.Division(432.23f, 5423.2f));
+            Console.ReadKey();
+        }
+    }
+    class SimpleMath
+    {
+        public static float Add(float n1,float n2)
+        {
+            return n1 + n2;
+        }
+        public static float Substract(float n1, float n2)
+        {
+            return n1 - n2;
+        }
+        public static float Multiplication(float n1, float n2)
+        {
+            return n1 * n2;
+        }
+        public static float Division(float n1, float n2)
+        {
+            return n1 / n2;
         }
     }
 }
