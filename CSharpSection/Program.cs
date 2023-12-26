57b227057b339cc36b476b4cdd0eec1bac089536
BakiDag <73464615+BakiDag@users.noreply.github.com>
2023-12-26 23:23:44 +0100

    [DEV] remove paramters

--- CSharpSection/Program.cs
+++ CSharpSection/Program.cs
@@ -9,39 +9,39 @@ namespace CSharpSection
         {
             Console.WriteLine("Hello World!");
 
-            int addResult = Add(n1, n2);
+            int addResult = Add();
             Console.WriteLine($"addResult: {addResult}");
-            int substractResult = Substract(n1, n2);
+            int substractResult = Substract();
             Console.WriteLine($"substractResult: {substractResult}");
-            double multiplyResult = Multiply(n1, n2);
+            double multiplyResult = Multiply();
             Console.WriteLine($"multiplyResult: {multiplyResult}");
-            float divideResult = Divide(n1, n2);
+            float divideResult = Divide();
             Console.WriteLine($"divideResult: {divideResult}");
             Console.ReadKey();
         }
 
-        private static int Add(int n1, int n2)
+        private static int Add()
         {
             int result;
             result = n1 + n2;
 
             return result;
         }
-        private static int Substract(int n1, int n2)
+        private static int Substract()
         {
             int result;
             result = n2 - n1;
 
             return result;
         }
-        private static double Multiply(int n1, int n2)
+        private static double Multiply()
         {
             int result;
             result = n1 * n2;
 
             return result;
         }
-        private static float Divide(int n1, int n2)
+        private static float Divide()
         {
             
             float result = (float)n2 / (float)n1;
