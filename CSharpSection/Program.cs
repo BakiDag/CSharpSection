b1df65a9eaca2b6d71f562d915d91c7ac5e9897e
BakiDag <73464615+BakiDag@users.noreply.github.com>
2023-12-26 23:21:18 +0100

    [DEV] implementing my own solution of the first challenge

--- CSharpSection/Program.cs
+++ CSharpSection/Program.cs
@@ -9,8 +9,14 @@ namespace CSharpSection
         {
             Console.WriteLine("Hello World!");
 
-            var result = Add(n1, n2);
-            
+            int addResult = Add(n1, n2);
+            Console.WriteLine($"addResult: {addResult}");
+            int substractResult = Substract(n1, n2);
+            Console.WriteLine($"substractResult: {substractResult}");
+            double multiplyResult = Multiply(n1, n2);
+            Console.WriteLine($"multiplyResult: {multiplyResult}");
+            float divideResult = Divide(n1, n2);
+            Console.WriteLine($"divideResult: {divideResult}");
             Console.ReadKey();
         }
 
@@ -18,6 +24,27 @@ namespace CSharpSection
         {
             int result;
             result = n1 + n2;
+
+            return result;
+        }
+        private static int Substract(int n1, int n2)
+        {
+            int result;
+            result = n2 - n1;
+
+            return result;
+        }
+        private static double Multiply(int n1, int n2)
+        {
+            int result;
+            result = n1 * n2;
+
+            return result;
+        }
+        private static float Divide(int n1, int n2)
+        {
+            
+            float result = (float)n2 / (float)n1;
 
             return result;
         }
